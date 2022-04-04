72ff43f6a80bfa76473ccb1fd9d893ec27627d42
Penelope Durand <penelope.durand@mail.goucher.edu>
2022-04-04 17:20:16 -0400

    commit tree c9c7da756edef4e7ac422888b7c9158e85ec92bd

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someNumber){
   // Write your code here!
+  let result 
+  //if....
+  if (someNumber <= 400) {
+    return `This one is on me!`
+  } else if (someNumber < 2000) {
+    return `That will be twenty bucks.`
+  } else if (someNumber > 2000) {
+    return `I will gladly take your thirty bucks.`
+  } else (someNumber >= 2500)
+    return `No can do.`
+}
+  
+function ternaryCheckCity(someCity){
+  // Write your code here!
+if (someCity === 'NYC') {
+  return `Ok, sounds good.`
+} else (someCity !== 'NYC') 
+  return `No go.`
 }
 
-function ternaryCheckCity(){
+function switchOnCharmFromTip(someTip){
   // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+  if (someTip === 'generous') {
+    return `Thank you so much.`
+  } else if (someTip === 'not as generous') {
+    return `Thank you.`
+  } else (someTip) 
+    return `Bye.`
 }
